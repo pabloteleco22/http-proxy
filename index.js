a03a1b7300f0fc8d2eab4e1b8acab49782e74ff8
Pablo López <p.lopez@cleverpy.com>
2023-12-07 10:00:18 +0100

    Added error control

--- index.js
+++ index.js
@@ -15,21 +15,44 @@ const TARGET_URL = `http://${TARGET_HOST}:${TARGET_PORT}`;
 
 const proxy = httpProxy.createProxyServer();
 
+let id = 0;
+
+proxy.on('proxyRes', (proxyRes, req, res) => {
+    let body = [];
+
+    proxyRes.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    proxyRes.on('end', () => {
+        res.statusCode = proxyRes.statusCode;
+        res.setHeader('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN.join(', '));
+
+        if ((ACCESS_CONTROL_ALLOW_METHODS.includes('OPTIONS')) && (req.method === 'OPTIONS')) {
+            res.statusCode = 200;
+            res.setHeader('Access-Control-Allow-Methods', ACCESS_CONTROL_ALLOW_METHODS.join(', '))
+                .setHeader('Access-Control-Allow-Headers', ACCESS_CONTROL_ALLOW_HEADERS.join(', '));
+        }
+        res.end(Buffer.concat(body).toString());
+    });
+});
+
+proxy.on('error', function (err, req, res) {
+    console.log(`Error on request ${req.reqId}`);
+  res.statusCode = 502;
+ 
+  res.end('Something went wrong');
+});
+
 http.createServer((req, res) => {
-    console.log(req.method);
+    req.reqId = id;
+    ++id;
+    console.log(`${req.reqId} - ${req.method}`);
     console.log(req.url);
     console.log();
 
     proxy.web(req, res, {
-        target: TARGET_URL
+        target: TARGET_URL,
+        selfHandleResponse: true
     });
-
-    res.setHeader('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN.join(', '));
-
-    if ((ACCESS_CONTROL_ALLOW_METHODS.includes('OPTIONS')) && (req.method === 'OPTIONS')) {
-        res.statusCode = 200;
-        res.setHeader('Access-Control-Allow-Methods', ACCESS_CONTROL_ALLOW_METHODS.join(', '))
-            .setHeader('Access-Control-Allow-Headers', ACCESS_CONTROL_ALLOW_HEADERS.join(', '))
-            .end();
-    }
 }).listen(PROXY_PORT);
